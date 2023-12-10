4ed9c4047b76418144ae4a5482b6b3ae7ef98fae
Firah Putri Pratiwi <firahputripratiwi10@gmail.com>
2023-12-10 15:33:40 +0700

    revisi tugas 1

--- automation/tugas01_1.py
+++ automation/tugas01_1.py
@@ -1,27 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 driver = webdriver.Chrome()
 
 
 #get url
-# driver.get('https://www.tiket.com')
-# sleep(2)
-# driver.get('https://www.tokopedia.com/')
-# sleep(2)
-# driver.get('https://www.orangsiber.com/')
-# sleep(2)
-# driver.get('https://automatetheboringstuff.com/')
-# driver.close()
-
-# while driver is open:
-#     print(driver.title)
 # tiket = 'https://www.tiket.com/'
 # tokped = 'https://www.tokopedia.com/'
 # orangSiber = 'https://www.orangsiber.com/'
 # automateBoring = 'https://automatetheboringstuff.com/'
+
+
+driver = webdriver.Chrome()
+
+url = ['https://www.tiket.com/', 'https://www.tokopedia.com/', 'https://www.orangsiber.com/'
+       'https://automatetheboringstuff.com/']
+
+
+
+
+
+# driver.get(tiket)
+# try
+#     WebDriverWait(driver,5).until(EC.title_contains('tiket.com'))
+#     Title = driver.title
+#     print(Title)
+    
+# except TimeoutException:
+#     print('Tidak ditemukan')
+#     pass
+
+
 
